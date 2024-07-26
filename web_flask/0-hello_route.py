f68702edde18a1ab98d80e681a3cbe1044638e74
R Sass <83635268+bluegalaxy13@users.noreply.github.com>
2024-07-26 15:36:16 +0200

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,17 +1,14 @@
-#!/usr/bin/python3
-"""Starts a Flask web application
-"""
+# web_flask/0-hello_route.py
 from flask import Flask
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
-@app.route('/', strict_slashes=False)
-def home():
-    """
-    Displays 'Hello HBNB!'
-    """
+@app.route('/')
+def hello():
+    """Displays 'Hello HBNB!'"""
     return 'Hello HBNB!'
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
     
