e453ac9248a97cd464d99a3889aeb62808e604a4
Ricardo Sass <ricardosassofficial@gmail.com>
2024-07-28 13:38:36 +0200

    added more answers for web AirBnB

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+"""_summary_
+"""
+from flask import Flask, render_template
+from models.state import State
+from models import storage
+
+app = Flask(__name__)
+
+@app.teardown_appcontext
+def teardown(exception):
+    """
+
+    Args:
+        exception (_type_): _description_
+    """
+    storage.close()
+    
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """
+    
+    """
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
+    
