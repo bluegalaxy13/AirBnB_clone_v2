6e9ff79acb21fbca2e09a8b03d468da018ccbe1d
R Sass <83635268+bluegalaxy13@users.noreply.github.com>
2024-07-28 12:17:03 +0000

    finished tasks

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -12,17 +12,17 @@ def teardown(exception):
     """
 
     Args:
-        exception (_type_): _description_
+        exception (_type_): Exception
     """
     storage.close()
     
-@app.route('/states_list', strict_slashes=False)
+@app.route('/cities_by_states', strict_slashes=False)
 def states_list():
     """
     
     """
     states = storage.all(State)
-    return render_template('7-states_list.html', states=states)
+    return render_template('8-cities_by_states.html', states=states)
     
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
