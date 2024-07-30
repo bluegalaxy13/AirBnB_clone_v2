762c0391e0e6f8faddaeae4351808f256a71ad17
ricardo <ricardosassofficial@gmail.com>
2024-07-30 13:03:11 +0000

    Changed first task in web_flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python3
 """
-starts a Flask web application
+This module starts a Flask web application that listens on
+0.0.0.0, port 5000, and responds with "Hello HBNB!" to
+requests at the root route.
 """
 
 from flask import Flask
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def index():
-    """returns Hello HBNB!"""
-    return 'Hello HBNB!'
+def hello():
+    """
+    Responds with "Hello HBNB!" when the root URL is accessed.
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    Returns:
+        str: The message "Hello HBNB!".
+    """
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
